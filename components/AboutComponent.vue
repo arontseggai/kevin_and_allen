c27194628d3fc72481853e06ca1d79b98b3c2f1d
arontseggai <aron.tseggai@gmail.com>
2018-06-15 13:13:17 +0200

    Adds something

--- components/AboutComponent.vue
+++ components/AboutComponent.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="columns">
     <div class="column is-4">
-      <p>GRYGIERCZYK & BOITELLE</p>
-      <br>
-      <div class="subcontent">
-        <p>Freelance film producer + freelance film director = amazing film duo.</p>
-        <br>
-        <p>
-          Bla, bla, creative, youth culture, millenials, bla, disruptive, bla, bla, bla, storytelling, bla, boundaries, content, bla, new media, bla, bla, pop culture, bla, high standards, bla, bla, talent, brands, bla, experience, bla, bla, bla, craftmanship, bla, knowledge, out of the box, bla.
-        </p>
-        <br>
-        <p>You know the story by know, we understand all of it.</p>
-        <br>
-        <p>Contact  <a href="mailto:info@grygboit.com">us</a> and lets make amazing work.</p>
+      <div class="outer-container">
+        <div class="inner-container">
+          <p>GRYGIERCZYK & BOITELLE</p>
+          <br>
+          <div class="subcontent">
+            <p>Freelance film producer + freelance film director = amazing film duo.</p>
+            <br>
+            <p>
+              Bla, bla, creative, youth culture, millenials, bla, disruptive, bla, bla, bla, storytelling, bla, boundaries, content, bla, new media, bla, bla, pop culture, bla, high standards, bla, bla, talent, brands, bla, experience, bla, bla, bla, craftmanship, bla, knowledge, out of the box, bla.
+            </p>
+            <br>
+            <p>You know the story by now, we understand all of it.</p>
+            <br>
+            <p>Contact  <a href="mailto:info@grygboit.com">us</a> and let's make amazing work.</p>
+          </div>
+        </div>
       </div>
     </div>
     <div class="column is-6 is-offset-2">
@@ -53,18 +57,24 @@
 </template>
 
 <style scoped>
+.outer-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+}
 
-  .imagery { position: relative; }
-  .imagery .hover {
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
-  }
+.imagery { position: relative; }
+.imagery .hover {
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+}
 
-  .imagery:hover .hover {
-    opacity: 1;
-  }
+.imagery:hover .hover {
+  opacity: 1;
+}
 
 
 
